51e5b128b422d37d7f7904cb2ceadc2f6629eaf6
SatyamWats <71392434+SatyamWats@users.noreply.github.com>
2022-05-15 13:01:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express=require('express')
 const axios=require('axios')
 const cheerio=require('cheerio')
-const fetch=require('node-fetch')                                       //node-fetch@2.0.0
+const fetch=require('node-fetch')  //node-fetch@2.0.0
+const { FunctionInstance } = require('twilio/lib/rest/serverless/v1/service/function')
+
+var chainIDArray=[]
+var tokenAddressArray=[]
+
+ console.log("API script started")
+
+//----------------------------------------------------------------------------------------------------------------
 
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '5357366311:AAGE-nYqdyT4f6O0gA2tV-zDkjWwGGS1TGg';
+const token = require("./key-info").token
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-const binanceAPIKey='DFAYPMDXCQSTTDIP8K5VZNUU4SPNMD5TUA'
-const covalentAPIKey='ckey_285dfce5b86d4797983378f58c3'
-const cmcAPIKey='4a71e39b-bc01-4cb8-8537-8adc5ac2c292'
-const apiEndpointbase='https://api.covalenthq.com/v1'
+const covalentAPIKey=require("./key-info").covalentAPIKey
+const apiEndpointBase='https://api.covalenthq.com/v1'
 
-const desiredPercentageChange=0                                         //desired percentage change for which the script should send alert
-const chainIDArray=['/56']                                              //has chain ID of all the tokens
-const tokenAddressArray=['/0x333fd139caef6aa31056cc905987b77b1044d259'] //has token addresses of all the tokens
+const desiredPercentageChange=0  
 
 var listSize='10000000'                                                 //total size of the JSON data list fetched from API
 
 var tokenHoldersArray=[]                                                //Number of token holders of all the tokens
 
-const chatID=1735223495
+const chatID=require("./key-info").chatID
 
 function sendAlert(change, percentageDifferenceOfHolders, lastAmount, currentAmount){
     
@@ -46,56 +50,71 @@ async function numberOfHolders(apiEndpoint) {
         return response.json()
     })
     .then(function(data){
-        // return data.data.pagination.total_count
         try{
-            //no of holders, price, volume, message on percentage change in price and holders and volume every 5 min
-            //console.log (data.data.pagination.total_count)
-            holders=data.data.pagination.total_count
+            holders=data.data.items.length
+            console.log("API Called")
             return holders
 
         }catch(err){
-            console.log(err)
+            console.log("error: "+err)
         }        
     })
 }
 
+//----------------------------------------------------------------------------------------------------------------------------
+
 async function main(){
 
-    var totalTokens=tokenAddressArray.length
+    console.log("API main function reached")
 
-    for(let index=0 ; index < totalTokens ; index++){
+    chainIDArray=require("./app").chainIDArray
+    tokenAddressArray=require("./app").tokenAddressArray
 
-        const chainID=chainIDArray[index]
-        const tokenAddress=tokenAddressArray[index]
+    if(chainIDArray != undefined || tokenAddressArray != undefined){
+        var totalTokens=tokenAddressArray.length
 
-        const apiEndpoint=apiEndpointbase+chainID+'/tokens'+tokenAddress+
-                          '/token_holders/?quote-currency=USD&format=JSON&page-size='+
-                          listSize+'&page-number=0&key='+covalentAPIKey
+        for(let index=0 ; index < totalTokens ; index++){
+            console.log("Entered loop")
+
+            const chainID="/"+chainIDArray[index]
+            const tokenAddress="/"+tokenAddressArray[index]
+
+            const apiEndpoint=apiEndpointBase+chainID+'/tokens'+tokenAddress+
+                              '/token_holders/?quote-currency=USD&format=JSON&page-size='+
+                              listSize+'&page-number=0&key='+covalentAPIKey
+
+            console.log("API Endpoint: "+ apiEndpoint)
     
-        var totalHoldersOfCurrentToken=await numberOfHolders(apiEndpoint) 
-        console.log(totalHoldersOfCurrentToken)
+            var totalHoldersOfCurrentToken=await numberOfHolders(apiEndpoint) 
 
-        if(tokenHoldersArray.length < totalTokens)  tokenHoldersArray.push(totalHoldersOfCurrentToken) 
-        else{
+            console.log("API Endpoint: "+apiEndpoint)
+            console.log("total holders of current token: "+totalHoldersOfCurrentToken)
 
-            var percentageDifferenceOfHolders = (( totalHoldersOfCurrentToken - tokenHoldersArray[index])/tokenHoldersArray[index])*100
-            if(Math.abs(percentageDifferenceOfHolders) >= desiredPercentageChange){
+            if(tokenHoldersArray.length < totalTokens)  tokenHoldersArray.push(totalHoldersOfCurrentToken) 
+            else{
 
-                let change=""
+                var percentageDifferenceOfHolders = (( totalHoldersOfCurrentToken - tokenHoldersArray[index])/tokenHoldersArray[index])*100
+                if(Math.abs(percentageDifferenceOfHolders) >= desiredPercentageChange){
 
-                if(percentageDifferenceOfHolders < 0) change="DECREASE"
-                else    change="INCREASE"
+                    let change=""
 
-                sendAlert(change, percentageDifferenceOfHolders, tokenHoldersArray[index], totalHoldersOfCurrentToken)
+                    if(percentageDifferenceOfHolders < 0) change="DECREASE"
+                    else    change="INCREASE"
+
+                    sendAlert(change, percentageDifferenceOfHolders, tokenHoldersArray[index], totalHoldersOfCurrentToken)
+
+                }
+
+                tokenHoldersArray[index] = totalHoldersOfCurrentToken //temporary statement
 
             }
 
         }
-
-        tokenHoldersArray[index] = totalHoldersOfCurrentToken
-
     }
+    
     
 }
 
-setInterval(main,1000*5)
+//----------------------------------------------------------------------------------------------------------------------------------
+
+ setInterval(main,1000*5)
